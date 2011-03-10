6438bb657e659453ab1b0698ee69fde54a98bd4e
Francois Marier <francois@debian.org>
2011-03-10 22:23:18 +1300

    Add basic plupload support

--- plupload_sample/upload/views.py
+++ plupload_sample/upload/views.py
@@ -1,5 +1,5 @@
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_protect
@@ -14,7 +14,14 @@ def upload_file(request):
             uploaded_file = request.FILES['file']
             form.save(uploaded_file)
 
-            return HttpResponseRedirect(reverse('plupload_sample.upload.views.upload_file'))
+            if request.is_ajax():
+                response = HttpResponse('{"jsonrpc" : "2.0", "result" : null, "id" : "id"}', mimetype='text/plain; charset=UTF-8')
+                response['Expires'] = 'Mon, 1 Jan 2000 01:00:00 GMT'
+                response['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0'
+                response['Pragma'] = 'no-cache'
+                return response
+            else:
+                return HttpResponseRedirect(reverse('plupload_sample.upload.views.upload_file'))
     else:
         form = UploadForm()
 
--- plupload_sample/urls.py
+++ plupload_sample/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls.defaults import *
 
+from plupload_sample import settings
+
 urlpatterns = patterns('',
     (r'^$', 'plupload_sample.upload.views.upload_file'),
+    (r'^css/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': '%s/static/css' % settings.PROJECT_ROOT}),
+    (r'^js/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': '%s/static/js' % settings.PROJECT_ROOT}),
 )
